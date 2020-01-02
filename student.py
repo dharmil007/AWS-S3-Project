71216045862f394f06548601f7f55670983455da
Dharmil Sanghvi <dharmil007@Gmail.com>
2020-01-01 19:21:13 -0500

    completed student

--- student.py
+++ student.py
@@ -1,6 +1,11 @@
 """ This programme creates a test source for Data & stores taht Data in a SQl FIle """
 
+import json
+import mysql.connector
+
+
 class student:
+
     def __init__ (self, name, SID, sub1, sub2, sub3, attn1, attn2, attn3, fees):
         self.name = name
         self.SID = SID
@@ -19,7 +24,7 @@ class student:
         """ Compute grades on 3 subject marks """
         totalMarks = 300
         obtainedMarks = ((self.sub1+self.sub2+self.sub3)/totalMarks)*100
-        print (obtainedMarks)
+        #print (obtainedMarks)
 
         if obtainedMarks > 90:
             self.gradesObtained = "A"
@@ -54,19 +59,51 @@ class student:
             self.results = "FAil"
 
         return self.results
-
+    """
     def convertJson (self, object):
-        """ Converts the result into jSon """
         print ("\n")
-        print (object.name+":")
+        #print (object.name+":")
         object.attendance()
-        print (object.attendanceTaken)
+        #print (object.attendanceTaken)
         object.grades()
-        print (object.gradesObtained)
+        #print (object.gradesObtained)
         object.result()
-        print (object.results)
-        print ("=====================================")
-        
+        #print (object.results)
+        #print ("=====================================")
+        object.mySqlConnection()
+    """
+
+    def mySqlConnection (self,object):
+        try:
+            myDB = mysql.connector.connect(
+                host = "dharmilsanghvi-com.cm2pz0cmh9kp.us-east-1.rds.amazonaws.com",
+                user = "student",
+                passwd = "g#ZNE33",
+                database = "test_stud"
+            )
+        except Exception as u:
+            print ("CAnnot Conect to databsae"+u)
+
+        object.grades()
+        object.attendance()
+        object.result()
+
+        mycursor = myDB.cursor()
+
+        try:
+            sqlCreate = "CREATE TABLE IF NOT EXISTS stud_info (STUD_id varchar(255) primary key, name varchar(255) not null, grades varchar(255) not null, attendance varchar(255) not null, result varchar (255) not null, fees varchar(255) not null)"
+            mycursor.execute (sqlCreate)
+        except Exception as o:
+            print ("Can't create a table"+o)
+        try:
+            sqlInsert = "INSERT INTO stud_info(STUD_ID, NAME, GRADES, ATTENDANCE, RESULT, FEES) VALUES (%s, %s, %s, %s, %s, %s)"
+            insertVals = (object.SID, object.name, object.gradesObtained, object.attendanceTaken, object.results, object.feesPaid)
+
+            mycursor.execute (sqlInsert,insertVals)
+            myDB.commit()
+        except Exception as i:
+            print ("Cannot insert data"+i)
+
 
 
 stu1 = student ("Sagar", "S01", 75, 85, 74, 50, 12, 78, 5000)
@@ -75,11 +112,11 @@ stu3 = student ("Deepti", "S03", 89,78,45,36,45,69,10000)
 stu4 = student ("Riddhi", "S04", 45,89,12,89,87,65,98000)
 stu5 = student ("Niyati", "S05", 89,78,54,3,56,98,99000)
 
-stu1.convertJson(stu1)
-stu2.convertJson(stu2)
-stu3.convertJson(stu3)
-stu4.convertJson(stu4)
-stu5.convertJson(stu5)
+stu1.mySqlConnection(stu1)
+stu2.mySqlConnection(stu2)
+stu3.mySqlConnection(stu3)
+stu4.mySqlConnection(stu4)
+stu5.mySqlConnection(stu5)
 
 
 #gradesObtained = stu1.grades()
